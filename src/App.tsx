9c6edd053f173a0942a70eb6ec0c09b10e184198
LGalushka <liliya2615@gmail.com>
2026-02-02 18:13:47 +0300

    feat(ui): add Icon component with basic emoji icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Icon from "./components/Icon"
 
 function App() {
   
   return (
     <>
-      
+      <Icon name='search' size={22} color="blue" />
     </>
   )
 }
---
+++ src/components/Icon.tsx
@@ -0,0 +1,25 @@
+const iconsData = {
+    home: '🏠',
+    search: '🔍',
+    user: '👤',
+    settings: '⚙️',
+  } as const;
+
+  type IconName = keyof typeof iconsData;
+
+  type IconProps = {
+  name: IconName;
+  size?: number;
+  color?: string;
+  }
+
+  const Icon = ({ name, size = 24, color = 'black'}: IconProps) => {
+    return (
+      <div style={{ color, fontSize: size, display: 'inline-block'}}>
+        {iconsData[name]}
+      </div>
+    )
+  }
+    
+
+export default Icon;
