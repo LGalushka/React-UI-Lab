b56a66078d972d9552b380f501ae507dee080bd2
LGalushka <liliya2615@gmail.com>
2026-02-03 23:26:38 +0300

    'feaf(layout): add Footer to MainLayout with styles'

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,12 +1,41 @@
+import { GitBranchIcon, Globe, MailIcon } from 'lucide-react';
 import styles from './Footer.module.css'
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className={styles.footer}>
-      <p>&copy; 2026 LABBOX. All rights reserved.</p>
-      <div className={styles.links}>
-        <a href='#'>Документация</a>
-        <a href="#">Поддержка</a>
+      <div className={styles.content}>
+        <div className={styles.brandSection}>
+          <h3 className={styles.logo}>LAB<span>BOX</span></h3>
+          <p className={styles.description}>
+            Современная среда для тестирования и разработки интерфейсов.
+          </p>
+        </div>
+
+        <div className={styles.linkSection}>
+          <div className={styles.column}>
+            <h4>Продукт</h4>
+              <a href="#">Компоненты</a>
+              <a href="#">Документация</a>
+          </div>
+          <div className={styles.column}>
+            <h4>Компания</h4>
+            <a href="#">О нас</a>
+            <a href="#">Контакты</a>
+          </div>
+        </div>
+
+        <div className={styles.socials}>
+          <GitBranchIcon size={20} className={styles.icon} />
+          <Globe size={20} className={styles.icon} />
+          <MailIcon size={20} className={styles.icon} />
+        </div>
+      </div>
+
+      <div className={styles.copyright}>
+        <p>&copy; {currentYear} LABBOX. Все права защищены.</p>
       </div>
     </footer>
   );
