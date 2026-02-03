6beccc44e241cba6a6310fdfb444298c1559182f
LGalushka <liliya2615@gmail.com>
2026-02-03 17:03:19 +0300

    feat(layout): Configure MainLayout component and fix Sadebar styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 //import Header from './components/layout/Header/Header';
-import Sidebar from './components/layout/Sidebar/Sidebar';
 //import Footer from "./components/layout/Footer/Footer";
 import FeedbackForm from "./pages/FeedbackRorm/FeedbackRorm";
 import SandboxPage from './pages/SandboxPage/SandboxPages';
+import { MainLayout } from "./components/layout/MainLayout/MainLayout";
 
 
 function App() {
@@ -12,24 +12,14 @@ function App() {
    { /** Главный контейнер на весь экран */}
    <div style={{ display: 'flex', minHeight: '100vh', backgroundColor: '#f9fafa'}}>
     
-    { /** Боковая панель (всегда на месте) */}
-    <Sidebar />
-
     {/** Основная рабочая область */}
-    <main style={{
-      flex: 1,
-      padding: '20px',
-      overflow: 'auto',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center'
-    }}>
+    <MainLayout>
       {/** Здесь происходит магия переключения */}
       <Routes>
         <Route path="/" element={<FeedbackForm />} />
         <Route path="/sandbox" element={<SandboxPage />} />
       </Routes>
-    </main>
+    </MainLayout>
 
    </div>
    </BrowserRouter>
