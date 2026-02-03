feda030c31838fb8bfa9d5d740f520a45e54445e
LGalushka <liliya2615@gmail.com>
2026-02-03 16:24:50 +0300

    feat: add statcard component and sandboxpage dashboard grid

--- src/pages/SandboxPage/SandboxPages.tsx
+++ src/pages/SandboxPage/SandboxPages.tsx
@@ -1,9 +1,38 @@
-const SandboxPages = () => {
+import React from "react";
+import { StatCard } from "../../components/ui/StatCard/StatCard";
+import styles from './SandboxPage.module.css';
+import { Users, Activity, AlertTriangle } from "lucide-react";
+
+const SandboxPages: React.FC = () => {
   return (
-    <>
-    <h1>Добро пожаловать в Песочницу!</h1>
-    </>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Панель управления</h1>
+
+      <div className={styles.grid}>
+        <StatCard
+        label="Всего пользователей"
+        value='1,284'
+        variant="primary"
+        icon={Users}
+        trend={{ value: 12, isPositive: true }}
+        />
+        <StatCard
+        label="Активные сессии"
+        value='432'
+        variant="success"
+        icon={Activity}
+        trend={{ value: 5, isPositive: true }}
+        />
+        <StatCard
+        label="Ошибки системы"
+        value='12'
+        variant='danger'
+        icon={AlertTriangle}
+        trend={{ value: 2, isPositive: false }}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default SandboxPages;
