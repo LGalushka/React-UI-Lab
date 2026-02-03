50184cc6b9c050488be2b77f401c00f465d26762
LGalushka <liliya2615@gmail.com>
2026-02-03 22:37:12 +0300

    feat(layout): add Header to MainLayout with styles

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,9 +1,15 @@
+import styles from './Footer.module.css'
+
 const Footer = () => {
   return (
-    <div>
-
-    </div>
-  )
-}
+    <footer className={styles.footer}>
+      <p>&copy; 2026 LABBOX. All rights reserved.</p>
+      <div className={styles.links}>
+        <a href='#'>Документация</a>
+        <a href="#">Поддержка</a>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
